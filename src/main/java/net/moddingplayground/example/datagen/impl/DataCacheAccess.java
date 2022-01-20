41ae7baccd8c3a3e0f227b5474a7ccc736f51426
andantet <orrdaniel73@gmail.com>
2022-01-20 16:01:38 +0000

    Initial commit

---
+++ src/main/java/net/moddingplayground/example/datagen/impl/DataCacheAccess.java
@@ -0,0 +1,9 @@
+package net.moddingplayground.example.datagen.impl;
+
+import java.nio.file.Path;
+
+@SuppressWarnings("unused")
+public interface DataCacheAccess {
+    void addCopyPath(Path out);
+    void keepFiles(Path... keep);
+}
---
+++ src/main/java/net/moddingplayground/example/datagen/impl/ObjectLootTableAccess.java
@@ -0,0 +1,7 @@
+package net.moddingplayground.example.datagen.impl;
+
+import net.minecraft.util.Identifier;
+
+public interface ObjectLootTableAccess {
+    Identifier access_getLootTableId();
+}
